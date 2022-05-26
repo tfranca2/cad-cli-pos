9cc458041c7a882f931738d3c8b24f05e1f1c398
Tiago <tfranca2@gmail.com>
2022-05-26 18:52:37 -0300

    getcanhoto obj

--- app/Lista.php
+++ app/Lista.php
@@ -9,4 +9,16 @@ class Lista extends Model
     protected $table = 'listas';
     protected $fillable = [ 'pasta', 'url', 'bilhete', 'cadastrado' ];
     protected $hidden = [ 'id', 'created_at', 'updated_at', 'deleted_at' ];
+
+    public static function getLastPasta()
+    {
+        return self::select('pasta')->distinct()->orderBy('pasta', 'DESC')->first()['pasta'];
+    }
+
+    public static function getCanhoto($pasta = '')
+    {
+        if( !$pasta )
+            $pasta = self::getLastPasta();
+        return self::inRandomOrder()->where('pasta', $pasta)->where('cadastrado', 0)->first();
+    }
 }
